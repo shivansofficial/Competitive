3472918249b26d633de2bef5e01a5855d1416d46
Parikshit Joon <ParikshitJ@Parikshits-MacBook-Pro.local>
2017-08-23 19:30:18 +0530

    first commmit

---
+++ temp.cpp
@@ -0,0 +1,39 @@
+#include<iomanip>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<map>
+#include<math.h>
+
+char cray[4] = {'^','>','v','<'}
+int det(char s)
+{
+  int i;
+  for(i=0;i<4;i++)
+  {
+    if(cray[i]==s)
+      break;
+  }
+  return i;
+}
+using namespace std;
+int main()
+{
+  // int t;
+  // cin>>t;
+  //
+  // while(t--)
+  // {
+  (< ,^)
+    char a,b;
+    cin>>a>>b;
+    int s,e;
+    s = det(a)
+    e = det(b)
+    int n;
+    cin>>n;
+    int pos = n%4;
+    s+pos
+  return 0;
+}
